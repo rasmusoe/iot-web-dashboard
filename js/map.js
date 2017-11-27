bb6d11357a9524211ad81d036934e6f607a80d3e
THP <mikku_pedersen@hotmail.com>
2017-11-27 11:50:54 +0100

    split dashboard into separate files and refactored parts of code to be easier to understand

---
+++ js/map.js
@@ -0,0 +1,27 @@
+/* --------- CREATE MAP --------- */
+function create_map(layer, path) {
+// Get JSON file and create the world on layer1 (Background)
+    d3.json("dk_ger_swe.json", function (error, topo) {
+
+        // VARIABLES
+        var subunits = topojson.feature(topo, topo.objects.subunits);
+        var subunits_features = topojson.feature(topo, topo.objects.subunits).features;
+        var places = topojson.feature(topo, topo.objects.places);
+        var places_features = topojson.feature(topo, topo.objects.places).features;
+
+        // GET ALL SUBUNITS FROM SHAPEFILE AND APPEND TO PATH
+        layer.selectAll(".subunit")
+            .data(subunits_features)
+            .enter().append("path")
+            .attr("class", function (d) {
+                return "subunit " + d.id;
+            })
+            .attr("d", path);
+
+        // GET PLACES (CITIES) FROM PLACES SHAPEFILE
+        layer.append("path")
+            .datum(places)
+            .attr("d", path)
+            .attr("class", "place");
+    });
+}
