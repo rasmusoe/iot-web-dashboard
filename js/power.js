c451b627cef78f8f4ebd43cdecdcd9c1dfc04748
THP <Woobs8@users.noreply.github.com>
2017-11-28 23:15:49 +0100

    added power plants to map

---
+++ js/power.js
@@ -0,0 +1,18 @@
+/* ----- POWER PLANTS ----- */
+function power_plant(svgGroup, coord, width, height, id) {
+    projectedCoords = projection(coord);
+
+    svgGroup.append("image")
+        .attr("height", height)
+        .attr("x", projectedCoords[0] - Math.round(width / 2))
+        .attr("y", projectedCoords[1] - Math.round(height / 2))
+        .attr("xlink:href", "img/power_plant.png")
+        .attr("id", id)
+}
+
+function create_power_plants(layer, coords) {
+    power_plant(layer, coords.sjaelland_power, 60, 60, 'powerplant');
+	power_plant(layer, coords.esbjerg_power, 60, 60, 'powerplant');
+	power_plant(layer, coords.studstrup_power, 60, 60, 'powerplant');
+	power_plant(layer, coords.aalborg_power, 60, 60, 'powerplant');
+}
